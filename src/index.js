f6d0b4eec8dc5cb76cae8befe499c69c0df5a73f
jaechung77 <jaechung77@gmail.com>
2021-07-03 21:27:39 +1000

    phase 1 final

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 
-
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('form').addEventListener('submit', e =>{
         e.preventDefault()
         let artistName = document.getElementById('singer').value
         fetchMusic(artistName)
+        
     })
 
 })
 
 function fetchMusic(artistName){
-    const tbody = document.getElementById('table-body')
+    const tbody = document.getElementById('result-body')
     while(tbody.firstChild){
         tbody.removeChild(tbody.lastChild)
     }
@@ -25,14 +25,39 @@ function fetchMusic(artistName){
       .then(items => {
           items.response.hits.forEach(hit =>{
               const elem = document.createElement('tr')
-              elem.innerHTML = `
+              const elemStr = `
               <td class='padding center'><img src=\"${hit.result.header_image_thumbnail_url}\" width=\"100px\" height=\"100px\"</td>
               <td class='padding center'>${hit.result.title}</td>
               <td class='padding center'><img src=\"${hit.result.primary_artist.image_url}\" width=\"100px\" height=\"100px\"</td>
-              <td class='padding center'>${hit.result.primary_artist.name}</td> 
-              <td class='padding center'>${hit.type}</td>                       
+              <td class='padding center'>${hit.result.primary_artist.name}</td>                      
               `
+              const btnStr= `<td class='padding center'><button class="btn btn-success">select</button></td>`     
+              elem.innerHTML = elemStr + btnStr
               tbody.appendChild(elem)
+
+              const button = elem.querySelector('button')
+              button.addEventListener('click', e =>{
+                elem.remove()
+                displayMyPlaylist(elemStr)
+              })
+
+
           })
       })
+}
+
+function displayMyPlaylist(elemStr){
+    const tbody = document.getElementById('mylist-body')    
+    const elem = document.createElement('tr')
+    const btnStr= `<td class='padding center'><button class="btn btn-danger">deselect</button></td>` 
+
+    elem.innerHTML = elemStr + btnStr
+    tbody.appendChild(elem)
+
+    const button = elem.querySelector('button')
+    button.addEventListener('click', e =>{
+        elem.remove()
+    })
+
+
 }
